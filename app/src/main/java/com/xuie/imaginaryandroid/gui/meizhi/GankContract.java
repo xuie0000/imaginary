4770efe6197895e16806330fb315c5de91afa748
Jie Xu <xujie@yf-space.com>
2017-07-05 18:17:21 +0800

    a ha

--- app/src/main/java/com/xuie/imaginaryandroid/gui/meizhi/GankContract.java
+++ app/src/main/java/com/xuie/imaginaryandroid/gui/meizhi/GankContract.java
@@ -2,6 +2,9 @@ package com.xuie.imaginaryandroid.gui.meizhi;
 
 import com.xuie.imaginaryandroid.BasePresenter;
 import com.xuie.imaginaryandroid.BaseView;
+import com.xuie.imaginaryandroid.data.MeiZhi;
+
+import java.util.List;
 
 /**
  * This specifies the contract between the view and the presenter.
@@ -10,7 +13,7 @@ public interface GankContract {
 
     interface View extends BaseView<Presenter> {
 
-        void addList(boolean isRefresh);
+        void addList(boolean isRefresh, List<MeiZhi> meiZhis);
     }
 
     interface Presenter extends BasePresenter {
--- app/src/main/java/com/xuie/imaginaryandroid/gui/meizhi/GankFragment.java
+++ app/src/main/java/com/xuie/imaginaryandroid/gui/meizhi/GankFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xuie.gzoomswiperefresh.GZoomSwipeRefresh;
 import com.xuie.imaginaryandroid.R;
+import com.xuie.imaginaryandroid.data.MeiZhi;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,10 +28,12 @@ import static com.xuie.imaginaryandroid.util.Utils.checkNotNull;
 public class GankFragment extends Fragment implements GankContract.View, GZoomSwipeRefresh.OnRefreshListener, GZoomSwipeRefresh.OnBottomRefreshListener {
     private static final String TAG = "GankFragment";
 
-    GankContract.Presenter mPresenter;
+    private GankContract.Presenter mPresenter;
     @BindView(R.id.recycle_view) RecyclerView recycleView;
     @BindView(R.id.swipe_refresh) GZoomSwipeRefresh swipeRefresh;
     Unbinder unbinder;
+
+    private MeiZhiAdapter meiZhiAdapter = new MeiZhiAdapter(null);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -56,8 +62,10 @@ public class GankFragment extends Fragment implements GankContract.View, GZoomSw
     }
 
     @Override
-    public void addList(boolean isRefresh) {
-
+    public void addList(boolean isRefresh, List<MeiZhi> meiZhis) {
+        if (isRefresh)
+            meiZhiAdapter.replaceData(null);
+        meiZhiAdapter.addData(meiZhis);
     }
 
     @Override
