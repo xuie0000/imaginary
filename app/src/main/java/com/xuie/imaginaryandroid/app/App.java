a6843742b16c18981888db1ea574ab5f00214af3
Jie Xu <xujie@yf-space.com>
2017-09-01 10:42:55 +0800

    添加Settings页

--- app/src/main/java/com/xuie/imaginaryandroid/app/App.java
+++ app/src/main/java/com/xuie/imaginaryandroid/app/App.java
@@ -2,6 +2,9 @@ package com.xuie.imaginaryandroid.app;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.v7.preference.PreferenceManager;
+
+import com.xuie.imaginaryandroid.R;
 
 /**
  * Created by xuie on 17-7-5.
@@ -15,6 +18,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
+        PreferenceManager.setDefaultValues(this, R.xml.fragment_settings, false);
     }
 
     public static Context getContext() {
--- app/src/main/java/com/xuie/imaginaryandroid/gui/settings/SettingsFragment.java
+++ app/src/main/java/com/xuie/imaginaryandroid/gui/settings/SettingsFragment.java
@@ -1,25 +1,81 @@
 package com.xuie.imaginaryandroid.gui.settings;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.xuie.imaginaryandroid.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "SettingsFragment";
+
     public static SettingsFragment getInstance() {
         return new SettingsFragment();
     }
 
+    public static final String KEY_LIST_PRE = "list_preference";
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference) {
+            preference.setSummary(sharedPreferences.getString(key, ""));
+        }
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.fragment_settings);
+        // 选项监听 ...
+        // findPreference("child_checkbox_preference").setOnPreferenceClickListener(...);
+        // 选项监听 当 Preference 的值发生改变时触发该事件，true则以新值更新控件的状态，false 则 不保存
+        findPreference("child_checkbox_preference").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                // 返回 false 修改不会生效
+                return false;
+            }
+        });
+        // 获取 Preferences Manager
+//        PreferenceManager manager = getPreferenceManager();
+        // 获取 选项状态
+//        CheckBoxPreference child_checkbox_preference = (CheckBoxPreference) manager.findPreference("child_checkbox_preference");
+        // ...
+//        Toast.makeText(getActivity(), String.valueOf(child_checkbox_preference.isChecked()), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 
 }
